ac2f1c4e3506008b9a54ab4ae5ae4e50e60e64f1
Krossi work <chrissiazor@gmail.com>
2026-06-24 11:14:41 +0200

    finished creating unsupervised priors in x dir finished reconstructions mem with unsupervised priors updated plots again

--- job_scripts/automated_conductivity_extraction.sh
+++ job_scripts/automated_conductivity_extraction.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 cd ~/Desktop/spec_rec_methods
-output="./dat/finite_T_reconstructions/econduct_B_z.dat"
+output="./dat/finite_T_reconstructions/mem_neural/econduct_B_x.dat"
 > "$output"   # clear output file
 
 
 for B in 0 2 4 6 8 10 12
 do
     cd ~/Desktop/spec_rec_methods/mem/outputs/emconduc_recs/neural_prior
-    file="./RhoOverOmega_finite_T_prior_file_data_wilson_emconduc_48_16_b6.872_B${B}_z.txt"
+    file="./RhoOverOmega_finite_T_prior_file_data_wilson_emconduc_48_16_b6.872_B${B}_x.txt"
     pwd
 
     value=$(awk 'NR==2 {print $2, $3}' "$file")
--- job_scripts/automated_reconstructions.sh
+++ job_scripts/automated_reconstructions.sh
@@ -3,5 +3,5 @@
 cd ~/Desktop/spec_rec_methods
 for B in 0 2 4 6 8 10 12
 do
-    python3 reconstruction.py --config params.json --default_model_file "../neuralFit/outputs/emconduc_recs/mem_priors/RhoOverOmega_data_wilson_emconduc_48_16_b6.872_B${B}_z.txt" --correlatorFile "../dat/data_wilson_emconduc_48_16_b6.872_B${B}_z.txt"
+    python3 reconstruction.py --config params.json --default_model_file "../neuralFit/outputs/emconduc_recs/mem_priors/RhoOverOmega_data_wilson_emconduc_48_16_b6.872_B${B}_x.txt" --correlatorFile "../dat/data_wilson_emconduc_48_16_b6.872_B${B}_x.txt"
 done
